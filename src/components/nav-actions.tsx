6e3fd2ed7633f5448413444e6e702fbff03ee790
Roy Quilor <royquilor@gmail.com>
2025-01-03 12:24:14 +0000

    Removed dropdown in nav actions

--- src/components/nav-actions.tsx
+++ src/components/nav-actions.tsx
@@ -1,118 +1,116 @@
 "use client"
 
-import * as React from "react"
-import {
-  ArrowDown,
-  ArrowUp,
-  Bell,
-  Copy,
-  CornerUpLeft,
-  CornerUpRight,
-  FileText,
-  GalleryVerticalEnd,
-  LineChart,
-  Link,
-  MoreHorizontal,
-  Settings2,
-  Trash,
-  Trash2,
-} from "lucide-react"
+// import * as React from "react"
+// import {
+//   ArrowDown,
+//   ArrowUp,
+//   Bell,
+//   Copy,
+//   CornerUpLeft,
+//   CornerUpRight,
+//   FileText,
+//   GalleryVerticalEnd,
+//   LineChart,
+//   Link,
+//   MoreHorizontal,
+//   Settings2,
+//   Trash,
+//   Trash2,
+// } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+// import { Button } from "@/components/ui/button"
+// import {
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+// } from "@/components/ui/popover"
+// import {
+//   Sidebar,
+//   SidebarContent,
+//   SidebarGroup,
+//   SidebarGroupContent,
+//   SidebarMenu,
+//   SidebarMenuButton,
+//   SidebarMenuItem,
+// } from "@/components/ui/sidebar"
 
-const data = [
-  [
-    {
-      label: "Customize Page",
-      icon: Settings2,
-    },
-    {
-      label: "Turn into wiki",
-      icon: FileText,
-    },
-  ],
-  [
-    {
-      label: "Copy Link",
-      icon: Link,
-    },
-    {
-      label: "Duplicate",
-      icon: Copy,
-    },
-    {
-      label: "Move to",
-      icon: CornerUpRight,
-    },
-    {
-      label: "Move to Trash",
-      icon: Trash2,
-    },
-  ],
-  [
-    {
-      label: "Undo",
-      icon: CornerUpLeft,
-    },
-    {
-      label: "View analytics",
-      icon: LineChart,
-    },
-    {
-      label: "Version History",
-      icon: GalleryVerticalEnd,
-    },
-    {
-      label: "Show delete pages",
-      icon: Trash,
-    },
-    {
-      label: "Notifications",
-      icon: Bell,
-    },
-  ],
-  [
-    {
-      label: "Import",
-      icon: ArrowUp,
-    },
-    {
-      label: "Export",
-      icon: ArrowDown,
-    },
-  ],
-]
+// const data = [
+//   [
+//     {
+//       label: "Customize Page",
+//       icon: Settings2,
+//     },
+//     {
+//       label: "Turn into wiki",
+//       icon: FileText,
+//     },
+//   ],
+//   [
+//     {
+//       label: "Copy Link",
+//       icon: Link,
+//     },
+//     {
+//       label: "Duplicate",
+//       icon: Copy,
+//     },
+//     {
+//       label: "Move to",
+//       icon: CornerUpRight,
+//     },
+//     {
+//       label: "Move to Trash",
+//       icon: Trash2,
+//     },
+//   ],
+//   [
+//     {
+//       label: "Undo",
+//       icon: CornerUpLeft,
+//     },
+//     {
+//       label: "View analytics",
+//       icon: LineChart,
+//     },
+//     {
+//       label: "Version History",
+//       icon: GalleryVerticalEnd,
+//     },
+//     {
+//       label: "Show delete pages",
+//       icon: Trash,
+//     },
+//     {
+//       label: "Notifications",
+//       icon: Bell,
+//     },
+//   ],
+//   [
+//     {
+//       label: "Import",
+//       icon: ArrowUp,
+//     },
+//     {
+//       label: "Export",
+//       icon: ArrowDown,
+//     },
+//   ],
+// ]
 
 export function NavActions() {
-  const [isOpen, setIsOpen] = React.useState(false)
+  // const [isOpen, setIsOpen] = React.useState(false)
 
-  React.useEffect(() => {
-    setIsOpen(true)
-  }, [])
+  // React.useEffect(() => {
+  //   setIsOpen(true)
+  // }, [])
 
   return (
     <div className="flex items-center gap-2 text-sm">
       <div className="hidden font-medium text-muted-foreground md:inline-block">
         Edit Oct 08
       </div>
-      {/* <Button variant="ghost" size="icon" className="h-7 w-7">
-        <Star />
-      </Button> */}
-      <Popover open={isOpen} onOpenChange={setIsOpen}>
+
+      {/* <Popover open={isOpen} onOpenChange={setIsOpen}>
         <PopoverTrigger asChild>
           <Button
             variant="ghost"
@@ -146,7 +144,7 @@ export function NavActions() {
             </SidebarContent>
           </Sidebar>
         </PopoverContent>
-      </Popover>
+      </Popover> */}
     </div>
   )
 }
